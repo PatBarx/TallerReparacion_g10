94f8b208871b9f05f85405a6f1f54e2725255b69
viruha <maurocastro1@gmail.com>
2022-12-01 20:20:35 -0300

    pruebas main clase repuesto

--- src/tallerreparacion_finalg10/TallerReparacion_FinalG10.java
+++ src/tallerreparacion_finalg10/TallerReparacion_FinalG10.java
@@ -14,11 +14,11 @@ public class TallerReparacion_FinalG10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Cliente clie =new Cliente(44,"anselmo marino","25 de mayo",134,true);  
-        ClienteData clieDa =new ClienteData();
-        Bicicleta bici = new Bicicleta(12,"benzo","mountain","celeste",clie,true); 
+     //   Cliente clie =new Cliente(44,"anselmo marino","25 de mayo",134,true);  
+     //   ClienteData clieDa =new ClienteData();
+     //   Bicicleta bici = new Bicicleta(12,"benzo","mountain","celeste",clie,true); 
         
-        BicicletaData biDa= new BicicletaData();
+     //   BicicletaData biDa= new BicicletaData();
 
   // biDa.altaBicicleta(bici);
      // biDa.bajaBicicleta(12);
@@ -40,6 +40,48 @@ public class TallerReparacion_FinalG10 {
             
         }*/
         
+        /*  CLASE RESPUESTO TESTING ******************************************************************
+        Repuesto rp1 = new Repuesto(123,"Repuesto 1", 200, true);
+        Repuesto rp2 = new Repuesto(456,"Repuesto 2", 300, true);
+        Repuesto rp3 = new Repuesto(789, "Repuesto 3", 400, true);
+        RepuestoData rd = new RepuestoData();
+        ArrayList<Repuesto> listarepuesto = null;
+
+        rd.altaRepuesto(rp1);
+        //System.out.println(rp1);
+        rd.altaRepuesto(rp2);
+        rd.altaRepuesto(rp3);
+
+        System.out.println("Lista Repuesto cargados");
+        listarepuesto = rd.listaRepuesto();
+        for (Repuesto repuesto : listarepuesto) {
+            System.out.println(repuesto);
+        }
+
+        rd.bajaRepuesto(rp1);
+
+        System.out.println("Lista Repuesto con una baja");
+        listarepuesto = rd.listaRepuesto();
+        for (Repuesto repuesto : listarepuesto) {
+            System.out.println(repuesto);
+        }
+        
+        rp3.setDescripcion("Repuesto 33");
+        
+        rd.modificacionRepuesto(rp3);
+        
+        System.out.println("Lista Repuesto con una modificacion");
+        listarepuesto = rd.listaRepuesto();
+        for (Repuesto repuesto : listarepuesto) {
+            System.out.println(repuesto);
+        }
+        
+        Repuesto repBuscado = rd.buscarRepuesto(456);
+        System.out.println("Repuesto Buscado");
+        System.out.println(repBuscado);
+        ****************************************************************************************************
+         */
+
 
     }
     
